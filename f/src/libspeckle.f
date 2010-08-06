fd61fb618782209629a292ace79a2b1b8daccb23
azzuro.huo <azzuro.huo@2b8b01a6-d071-d8e9-7859-68a7c242cbda>
2010-08-06 11:56:18 +0000

    isa subroutine is ready.

--- f/src/libspeckle.f
+++ f/src/libspeckle.f
@@ -170,7 +170,6 @@ C  location.
           ENDIF
         ENDDO
       ENDDO
-      WRITE(*,'(A,I8.0)') ' Number of points sampled:',NSAMPLES
       ALLOCATE(A(NSAMPLES,6))
       K=1
       DO J=1,N
@@ -255,7 +254,6 @@ C  BG is the output of this subroutine.
           ENDIF
         ENDDO
       ENDDO
-      WRITE (*,'(A,I8.0)')' Number of points sampled:',NSAMPLES
       ALLOCATE(A(NSAMPLES,NPARAMS))
       K=1
       DO J=1,N
@@ -335,6 +333,8 @@ C ******************************************************************************
         CALL BGFIT2P2(M,N,D,IMG,WORK,BUFFER)
       ELSE IF (FTMETHOD .EQ. 4) THEN
         CALL BGFIT2P4(M,N,D,IMG,WORK,BUFFER)
+      ELSE IF (FTMETHOD .EQ. 0) THEN
+        CALL BGFIT2P0(M,N,D,IMG,WORK)
       ELSE
         PRINT *,' Unknown fitting method.'
         RETURN
@@ -368,6 +368,8 @@ C ******************************************************************************
 C
 C
 C ******************************************************************************
+C
+C
       SUBROUTINE DECONVWNR(M,N,DG,DF,DH,DSNR)
 C  Wiener deconvolution subroutine.
 C
@@ -391,7 +393,6 @@ C  =============
       DOUBLE PRECISION :: DSNR
       DOUBLE COMPLEX :: ZG(M,N),ZF(M,N),ZH(M,N)
       DOUBLE COMPLEX :: ZCOMM(M*N+3*(M+N)),ZDECONV(M,N)
-      PRINT *,'1'
       ZG=CMPLX(DG)
       ZH=CMPLX(DH)
       CALL ZFFTSHIFT(M,N,ZH)
@@ -400,10 +401,14 @@ C  =============
       ZDECONV=CONJG(ZH)/(ZH*CONJG(ZH)+CMPLX(DBLE(1)/DSNR))
       ZF=ZG*ZDECONV
       CALL ZFFT2D(1,M,N,ZF,ZCOMM,INFO)
-      DF=REAL(ZF)
+      DF=DBLE(ZF)
       RETURN
       END SUBROUTINE DECONVWNR
+C
+C
 C ******************************************************************************
+C
+C
       SUBROUTINE ZFFTSHIFT(M,N,ZX)
 C  Shift zero-frequency component to centre of spectrum.
       INTEGER :: M,N,EM,SM
@@ -417,3 +422,177 @@ C  Shift zero-frequency component to centre of spectrum.
       ZX=ZY
       RETURN
       END SUBROUTINE ZFFTSHIFT
+C
+C
+C ******************************************************************************
+C
+C
+      SUBROUTINE SSAREC(INFILE,FPIXELS,LPIXELS,IMG,BG,WORK)
+      INTEGER :: FPIXELS(*),LPIXELS(*),NAXES(2)
+      INTEGER :: K,NPIXELS,NFRAMES,X,Y,XC,YC
+      DOUBLE PRECISION :: IMG(*),BG(*),DA,DSUM
+      DOUBLE PRECISION :: WORK(LPIXELS(1)-FPIXELS(1)+1,*)
+      CHARACTER*(*) :: INFILE
+      NAXES=(/LPIXELS(1)-FPIXELS(1)+1,LPIXELS(2)-FPIXELS(2)+1/)
+      NFRAMES=LPIXELS(3)-FPIXELS(3)+1
+      NPIXELS=NAXES(1)*NAXES(2)
+      XC=INT(CEILING(0.5*DBLE(NAXES(2)+1)))
+      YC=INT(CEILING(0.5*DBLE(NAXES(1)+1)))
+      CALL DSCAL(NPIXELS,DBLE(0),IMG,1)
+      DO K=FPIXELS(3),LPIXELS(3)
+          CALL READIMAGE(INFILE,(/FPIXELS(1),FPIXELS(2),K/),
+     &    (/LPIXELS(1),LPIXELS(2),K/),WORK)
+        DSUM=SUM(WORK(1:NAXES(1),1:NAXES(2)))
+        CALL DSCAL(NPIXELS,DBLE(NPIXELS)/DSUM,WORK,1)
+        CALL DAXPY(NPIXELS,DBLE(-1),BG,1,WORK,1)
+        X=MAXLOC(MAXVAL(WORK(1:NAXES(1),1:NAXES(2)),1),2)
+        Y=MAXLOC(MAXVAL(WORK(1:NAXES(1),1:NAXES(2)),2),1)
+        DSUM=SUM(WORK(1:NAXES(1),1:NAXES(2)))
+        CALL DSCAL(NPIXELS,DBLE(NPIXELS)/DSUM,WORK,1)
+        WORK(1:NAXES(1),1:NAXES(2))=EOSHIFT(
+     &    WORK(1:NAXES(1),1:NAXES(2)),X-XC,DBLE(0),2)
+        WORK(1:NAXES(1),1:NAXES(2))=EOSHIFT(
+     &    WORK(1:NAXES(1),1:NAXES(2)),Y-YC,DBLE(0),1)
+        CALL DAXPY(NPIXELS,DBLE(1),WORK,1,IMG,1)
+      END DO
+      CALL DSCAL(NPIXELS,DBLE(1)/DBLE(NFRAMES),IMG,1)
+      RETURN
+      END SUBROUTINE SSAREC
+C
+C
+C ******************************************************************************
+C
+C
+      SUBROUTINE ISAREC(OBSFILE,FPIXELS,LPIXELS,M,N,DREF,DBG,DISA,DR,
+     &  P,NUMIT,PREFIX)
+C
+C  Arguments:
+C  ==========
+C  OBSFILE - Observed fits file to process.
+C  FPIXELS - First pixels in each frame of the observed fits file.
+C  LPIXELS - Last pixels in each frame of the observed fits file.
+C  M       - Number of rows of DREF, DBG, and DISA.
+C  N       - Number of columns of DREF, DBG, and DISA.
+C  DREF    - Image of the reference star.
+C  DBG     - Background of the average of all frames of the observed fits file.
+C  DISA    - Input/Output. As input it is the initial estimate.
+C  DR      - Radius of the border of the signal in the image.
+C  P       - Pad all matrices to P x P for fast fourier transform.
+C  NUMIT   - Number of iterations.
+C  PREFIX  - Prefix of output filename.
+C
+C
+      INTEGER :: FPIXELS(*),LPIXELS(*),NUMIT,K,L,INFO,X,Y
+      INTEGER :: M,N,NPIXS,NUMFRM,P
+      DOUBLE PRECISION :: DSNR,DR,DRMS
+      DOUBLE PRECISION, DIMENSION(M,N) :: DIMG,DREF,DBG,DISA,WORK
+      DOUBLE PRECISION, DIMENSION(P,P) :: DCORR
+      DOUBLE COMPLEX,DIMENSION(P,P) :: ZIMG,ZISA,ZPDS
+      DOUBLE COMPLEX :: COMM(P*P+6*P+200)
+      CHARACTER*(*) :: PREFIX
+      CHARACTER :: OBSFILE*80,ISAFILE*80,NUMSTR*80
+      NPIXS=M*N
+      NUMFRM=LPIXELS(3)-FPIXELS(3)+1
+      DO L=1,NUMIT
+        WRITE(*,'(A,I4)') 'Loop =',L
+        WORK=0.0
+        ZISA=CMPLX(0.0)
+        ZISA(1:M,1:N)=CMPLX(DISA)
+        CALL ZFFT2D(-1,P,P,ZISA,COMM,INFO)
+        ZISA=CONJG(ZISA)
+        DO K=FPIXELS(3),LPIXELS(3)
+          CALL READIMAGE(OBSFILE,(/FPIXELS(1),FPIXELS(2),K/),
+     &      (/LPIXELS(1),LPIXELS(2),K/),DIMG)
+          DIMG=DIMG/SUM(DIMG)*DBLE(NPIXS)-DBG
+          DIMG=DIMG/SUM(DIMG)*DBLE(NPIXS)
+          ZIMG=CMPLX(0.0)
+          ZIMG(1:M,1:N)=CMPLX(DIMG)
+          CALL ZFFT2D(-1,P,P,ZIMG,COMM,INFO)
+          ZPDS=ZIMG*ZISA
+          CALL ZFFT2D(1,P,P,ZPDS,COMM,INFO)
+          DCORR=DBLE(ZPDS)
+C         CALL WRITEIMAGE('CORR.FITS',(/1,1,1/),(/P,P,1/),DCORR)
+          X=MAXLOC(MAXVAL(DCORR,1),2)
+          Y=MAXLOC(MAXVAL(DCORR,2),1)
+          IF (P-X.GT.X-1)THEN
+            X=X-1
+          ELSE
+            X=X-P
+          END IF
+          IF (P-Y.GT.Y-1)THEN
+            Y=Y-1
+          ELSE
+            Y=Y-P
+          END IF
+          WORK=WORK+EOSHIFT(EOSHIFT(DIMG,Y-1,DBLE(0),1),
+     &      X-1,DBLE(0),2)
+        END DO
+        WORK=WORK/DBLE(NUMFRM)
+        WRITE(NUMSTR,*) L
+        ISAFILE=TRIM(PREFIX)//'_ISA_'//TRIM(ADJUSTL(NUMSTR))//'.FITS'
+        CALL WRITEIMAGE(ISAFILE,(/1,1,1/),(/M,N,1/),WORK)
+        CALL GETSNR(M,N,DR,DSNR,WORK,0)
+        CALL DECONVWNR(M,N,WORK,DIMG,DREF,DSNR)
+        ISAFILE=TRIM(PREFIX)//'_ISA_DECONV_'//
+     &    TRIM(ADJUSTL(NUMSTR))//'.FITS'
+        CALL WRITEIMAGE(ISAFILE,(/1,1,1/),(/M,N,1/),DIMG)
+        DRMS=SQRT(SUM((DIMG-DISA)*(DIMG-DISA))/DBLE(NPIXS))
+        WRITE(*,'(A,A,ES10.3,A,ES10.3)') TRIM(ISAFILE),', RMS =',DRMS,
+     &    ' SNR =',DSNR
+        DISA=DIMG
+      END DO
+      RETURN
+      END SUBROUTINE
+C
+C
+C ******************************************************************************
+C
+C
+      SUBROUTINE DCORR2D(M,N,DA,DB,DC)
+C  correlation coefficients of 2-dimensional array.
+C
+C  Purpose:
+C  ========
+C  c = corr(a, b). calculates c given a and b using fast fourier transform.
+C  fft(c) = fft(a) * conj(fft(b)) (periodic extension is implied).
+C
+      INTEGER :: M,N,INFO
+      DOUBLE PRECISION, DIMENSION(M,N) :: DA,DB,DC
+      DOUBLE COMPLEX, DIMENSION(M,N) :: ZA,ZB,ZC
+      DOUBLE COMPLEX :: COMM(M*N+3*(M+N))
+      ZA=CMPLX(DA)
+      ZB=CMPLX(DB)
+      CALL ZFFT2D(-1,M,N,ZA,COMM,INFO)
+      CALL ZFFT2D(-1,M,N,ZB,COMM,INFO)
+      ZC=ZA*CONJG(ZB)
+      CALL ZFFT2D(1,M,N,ZC,COMM,INFO)
+      DC=DBLE(ZC)
+      RETURN
+      END SUBROUTINE DCORR2D
+C
+C
+C ******************************************************************************
+C
+C
+      SUBROUTINE BGFIT2P0(M,N,D,IMG,BG)
+      INTEGER :: NSAMPLES,LWORK,INFO,LDA,LDB
+      INTEGER :: M,N,I,J,K
+      DOUBLE PRECISION :: X,Y,XC,YC,D,B
+      DOUBLE PRECISION :: IMG(M,N),BG(M,N)
+      NSAMPLES=0
+      B=0
+      XC=0.5*(1+DBLE(N))
+      YC=0.5*(1+DBLE(M))
+      DO J=1,N
+        DO I=1,M
+          X=DBLE(J)
+          Y=DBLE(I)
+          IF (SQRT((X-XC)*(X-XC)+(Y-YC)*(Y-YC)).GE.D) THEN
+            NSAMPLES=NSAMPLES+1
+            B=IMG(I,J)
+          END IF
+        END DO
+      END DO
+      BG=B/DBLE(NSAMPLES)
+      RETURN
+      ENDSUBROUTINE BGFIT2P0

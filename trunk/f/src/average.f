dcf531d9837f4b56ef3e2ebeddf29ca6b10abdc9
azzuro.huo <azzuro.huo@2b8b01a6-d071-d8e9-7859-68a7c242cbda>
2010-08-19 03:29:20 +0000

    modified average.f.

--- trunk/f/src/average.f
+++ trunk/f/src/average.f
@@ -1,23 +1,61 @@
       PROGRAM MAIN
+C  Average images.
+C
+C  Usage:
+C  ======
+C  average filename_img [-fpixels=n,n,n] [-lpixels=n,n,n] [-o=filename_avg]
+C
+C  Purpose:
+C  ========
+C  take filename_img as input. average subimages defined by fpixels and lpixels
+C  and write the output to file named filename_avg.
+C
+C  Arguments:
+C  ==========
+C  filename_img - input
+C  n,n,n        - integers
+C  filename_avg - output
+C
+      IMPLICIT NONE
       INTEGER :: FPIXELS(3),LPIXELS(3),NAXES(3),K,NARGS
-      DOUBLE PRECISION, ALLOCATABLE :: ARRAY(:,:)
-      CHARACTER(LEN=256) :: FILENAME,ARG,AVGFILE,DATESTR
+      DOUBLE PRECISION, ALLOCATABLE :: DAVG(:,:)
+      CHARACTER(LEN=256) :: FILENAME,ARG,AVGFILE,BASENAME,EXTNAME
       INTERFACE
       INCLUDE 'ispeckle.f'
       END INTERFACE
       NARGS=COMMAND_ARGUMENT_COUNT()
+      CALL GET_COMMAND_ARGUMENT(1,FILENAME)
       CALL IMAGESIZE(FILENAME,NAXES)
-      CALL GET_COMMAND_ARG(1,FILENAME)
-      CALL GETARG(2,ARG)
-      READ(ARG,*) FPIXELS(1),FPIXELS(2),FPIXELS(3)
-      CALL GETARG(3,ARG)
-      READ(ARG,*) LPIXELS(1),LPIXELS(2),LPIXELS(3)
-      CALL GETARG(4,AVGFILE)
-      WRITE(*,'(2A)') ' Input: ',TRIM(FILENAME)
-      ALLOCATE(ARRAY(NPIXELS))
-      CALL AVERAGE(TRIM(FILENAME),FPIXELS,LPIXELS,ARRAY)
-      CALL WRITEIMAGE(TRIM(AVGFILE),(/1,1,1/),(/M,N,1/),ARRAY)
-      WRITE(*,'(2A)') ' Output: ',TRIM(AVGFILE)
-      DEALLOCATE(ARRAY)
+      FPIXELS=(/1,1,1/)
+      LPIXELS=NAXES
+      CALL RESOLVEPATH(FILENAME,BASENAME,EXTNAME)
+      AVGFILE=TRIM(BASENAME)//'_avg.fits'
+      DO K=2,NARGS
+        CALL GET_COMMAND_ARGUMENT(K,ARG)
+        IF(INDEX(ARG,'-fpixels=').GT.0)THEN
+          READ(ARG(INDEX(ARG,'-fpixels=')+9:),*)
+     &      FPIXELS(1),FPIXELS(2),FPIXELS(3)
+        ELSE IF(INDEX(ARG,'-lpixels=').GT.0)THEN
+          READ(ARG(INDEX(ARG,'-lpixels=')+9:),*)
+     &      LPIXELS(1),LPIXELS(2),LPIXELS(3)
+        ELSE IF(INDEX(ARG,'-o=').GT.0)THEN
+          AVGFILE=ARG(INDEX(ARG,'-o=')+3:)
+        ELSE
+          PRINT *,'Unknown argument '//TRIM(ARG)
+          RETURN
+        END IF
+      END DO
+      WRITE(*,'(A)') ' input: '//TRIM(FILENAME)
+      WRITE(*,'(A)') ' output: '//TRIM(AVGFILE)
+      WRITE(*,'(A,I3,A,I3)')
+     &  ' average subimage size (width x height): ',
+     &  LPIXELS(1)-FPIXELS(1)+1,' x',LPIXELS(2)-FPIXELS(2)+1
+      WRITE(*,'(A,I5,A,I5)') ' average from ',FPIXELS(3),
+     &  ' to ',LPIXELS(3)
+      ALLOCATE(DAVG(NAXES(1)*NAXES(2)))
+      CALL AVERAGE(TRIM(FILENAME),FPIXELS,LPIXELS,DAVG)
+      CALL WRITEIMAGE(TRIM(AVGFILE),(/1,1,1/),
+     &  (LPIXELS(1)+1-FPIXELS(1),LPIXELS(2)+1-FPIXELS(2),1/),DAVG)
+      DEALLOCATE(DAVG)
       STOP
       END PROGRAM

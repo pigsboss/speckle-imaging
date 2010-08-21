929f8fb7a44f60a9a9c7babfa7bb17e23e35c081
azzuro.huo <azzuro.huo@2b8b01a6-d071-d8e9-7859-68a7c242cbda>
2010-08-21 11:40:49 +0000

    fixed several bugs.

--- f/src/bgfit.f
+++ f/src/bgfit.f
@@ -1,7 +1,7 @@
       PROGRAM BGFIT
 C  Usage:
 C  ======
-C  bgfit filename_avg filename_bg [-r=radius] [-fit=fit_method]
+C  bgfit filename_avg [-o=filename_bg] [-r=radius] [-fit=fit_method]
 C
 C  Purpose:
 C  ========
@@ -19,8 +19,12 @@ C
       INTEGER :: NAXES(3),NPARAMS,K,NARGS
       DOUBLE PRECISION :: DR
       DOUBLE PRECISION, ALLOCATABLE :: DIMG(:,:),DBG(:,:),DPARAMS(:)
-      CHARACTER(LEN=256) :: AVGFILE,BGFILE,FTMETHOD,ARG
+      CHARACTER(LEN=256) :: AVGFILE,BGFILE,FTMETHOD,ARG,BASENAME,EXTNAME
       INTERFACE
+      SUBROUTINE RESOLVEPATH(PATH,BASENAME,EXTNAME)
+      CHARACTER*(*), INTENT(IN) :: PATH
+      CHARACTER*(*), INTENT(OUT) :: BASENAME,EXTNAME
+      END SUBROUTINE RESOLVEPATH
       SUBROUTINE BGFIT2P0(M,N,DR,DIMG,DBG,DPARAMS)
       INTEGER, INTENT(IN) :: M,N
       DOUBLE PRECISION, INTENT(IN) :: DR,DIMG(M,N)
@@ -41,17 +45,21 @@ C
       CHARACTER*(*) :: IMGFILE
       END SUBROUTINE IMAGESIZE
       END INTERFACE
+      NARGS=COMMAND_ARGUMENT_COUNT()
       CALL GET_COMMAND_ARGUMENT(1,AVGFILE)
-      CALL GET_COMMAND_ARGUMENT(2,BGFILE)
       CALL IMAGESIZE(AVGFILE,NAXES)
       DR=0.5*DBLE(MIN(NAXES(1),NAXES(2)))
-      FTMETHOD='p4'
-      DO K=3,NARGS
+      FTMETHOD='all'
+      CALL RESOLVEPATH(AVGFILE,BASENAME,EXTNAME)
+      BGFILE=TRIM(BASENAME)//'_bg'
+      DO K=2,NARGS
         CALL GET_COMMAND_ARGUMENT(K,ARG)
         IF(INDEX(ARG,'-r=').GT.0)THEN
-          READ(ARG(INDEX(ARG,'-r=')+3:256),*) DR
+          READ(ARG(INDEX(ARG,'-r=')+3:),*) DR
         ELSE IF(INDEX(ARG,'-fit=').GT.0)THEN
-          READ(ARG(INDEX(ARG,'-fit=')+5:256),*) FTMETHOD
+          FTMETHOD=ARG(INDEX(ARG,'-fit=')+5:)
+        ELSE IF(INDEX(ARG,'-o=').GT.0)THEN
+          BGFILE=ARG(INDEX(ARG,'-o=')+3:)
         ELSE
           PRINT *,'Unknown argument '//TRIM(ARG)
           RETURN
@@ -62,19 +70,64 @@ C
       ALLOCATE(DPARAMS(NAXES(2)*NAXES(1)))
       CALL READIMAGE(AVGFILE,(/1,1,1/),(/NAXES(1),NAXES(2),1/),DIMG)
       IF(FTMETHOD .EQ. 'p0')THEN
+        PRINT *,'constant average fit:'
         NPARAMS=1
         CALL BGFIT2P0(NAXES(2),NAXES(1),DR,DIMG,DBG,DPARAMS)
       ELSE IF(FTMETHOD .EQ. 'p2')THEN
+        PRINT *,'quadratic polynomials fit:'
         NPARAMS=6
         CALL BGFIT2P2(NAXES(2),NAXES(1),DR,DIMG,DBG,DPARAMS)
       ELSE IF(FTMETHOD .EQ. 'p4')THEN
+        PRINT *,'quartic polynomials fit:'
         NPARAMS=15
         CALL BGFIT2P4(NAXES(2),NAXES(1),DR,DIMG,DBG,DPARAMS)
+      ELSE IF(FTMETHOD .EQ. 'all')THEN
+        PRINT *,'constant average fit:'
+        NPARAMS=1
+        CALL BGFIT2P0(NAXES(2),NAXES(1),DR,DIMG,DBG,DPARAMS)
+        CALL WRITEIMAGE(TRIM(BGFILE)//'_p0.fits',
+     &    (/1,1,1/),(/NAXES(1),NAXES(2),1/),DBG)
+        PRINT *,'output: ',TRIM(BGFILE)//'_p0.fits'
+        DO K=1,NPARAMS
+          WRITE(ARG,*)K
+          WRITE(*,'(A,ES10.3)')' a_'//TRIM(ADJUSTL(ARG))//' = ',
+     &      DPARAMS(K)
+        END DO
+        PRINT *,'quadratic polynomials fit:'
+        NPARAMS=6
+        CALL BGFIT2P2(NAXES(2),NAXES(1),DR,DIMG,DBG,DPARAMS)
+        CALL WRITEIMAGE(TRIM(BGFILE)//'_p2.fits',
+     &    (/1,1,1/),(/NAXES(1),NAXES(2),1/),DBG)
+        PRINT *,'output: ',TRIM(BGFILE)//'_p2.fits'
+        DO K=1,NPARAMS
+          WRITE(ARG,*)K
+          WRITE(*,'(A,ES10.3)')' a_'//TRIM(ADJUSTL(ARG))//' = ',
+     &      DPARAMS(K)
+        END DO
+        PRINT *,'quartic polynomials fit:'
+        NPARAMS=15
+        CALL BGFIT2P4(NAXES(2),NAXES(1),DR,DIMG,DBG,DPARAMS)
+        CALL WRITEIMAGE(TRIM(BGFILE)//'_p4.fits',
+     &    (/1,1,1/),(/NAXES(1),NAXES(2),1/),DBG)
+        PRINT *,'output: ',TRIM(BGFILE)//'_p4.fits'
+        DO K=1,NPARAMS
+          WRITE(ARG,*)K
+          WRITE(*,'(A,ES10.3)')' a_'//TRIM(ADJUSTL(ARG))//' = ',
+     &      DPARAMS(K)
+        END DO
+        DEALLOCATE(DIMG)
+        DEALLOCATE(DBG)
+        DEALLOCATE(DPARAMS)
+        RETURN
       ELSE
         PRINT *,'Unknown fitting method '//TRIM(FTMETHOD)
+        DEALLOCATE(DIMG)
+        DEALLOCATE(DBG)
+        DEALLOCATE(DPARAMS)
         RETURN
       END IF
       CALL WRITEIMAGE(BGFILE,(/1,1,1/),(/NAXES(1),NAXES(2),1/),DBG)
+      PRINT *,'output: ',TRIM(BGFILE)
       DO K=1,NPARAMS
         WRITE(ARG,*)K
         WRITE(*,'(A,ES10.3)')' a_'//TRIM(ADJUSTL(ARG))//' = ',
